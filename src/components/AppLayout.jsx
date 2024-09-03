46d67daaf492f6c0cb15d4d68f761fe3f7934e57
Vamshi Krishna Kodimala <102660840+vkrishna03@users.noreply.github.com>
2024-09-03 14:27:38 +0530

    routes setup

---
+++ src/components/AppLayout.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+const AppLayout = () => {
+  return (
+    <>
+      <header></header>
+      <main>
+        <Outlet />
+      </main>
+      <footer></footer>
+    </>
+  );
+};
+
+export default AppLayout;
